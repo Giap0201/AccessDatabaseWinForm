8bf42909e9f317348a1ba3b63aa5ce28576bb2da
Nguyễn Hữu Giáp <giap13022005@@gmail.com>
2025-09-28 23:14:06 +0700

    Add project files.

---
+++ AccessDatabase/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace AccessDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            labFirstName = new Label();
+            labLastName = new Label();
+            labClass = new Label();
+            labEmail = new Label();
+            labAddress = new Label();
+            labAge = new Label();
+            txtFirstName = new TextBox();
+            txtLastName = new TextBox();
+            txtClass = new TextBox();
+            txtEmail = new TextBox();
+            txtAddress = new TextBox();
+            txtAge = new TextBox();
+            btnAdd = new Button();
+            btnUpdate = new Button();
+            btnDelete = new Button();
+            labId = new Label();
+            txtId = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = SystemColors.ActiveCaption;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(389, 53);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(940, 517);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick_1;
+            // 
+            // labFirstName
+            // 
+            labFirstName.AutoSize = true;
+            labFirstName.BackColor = SystemColors.ButtonFace;
+            labFirstName.Location = new Point(69, 123);
+            labFirstName.Name = "labFirstName";
+            labFirstName.Size = new Size(76, 20);
+            labFirstName.TabIndex = 1;
+            labFirstName.Text = "FirstName";
+            // 
+            // labLastName
+            // 
+            labLastName.AutoSize = true;
+            labLastName.BackColor = SystemColors.ButtonFace;
+            labLastName.Location = new Point(69, 187);
+            labLastName.Name = "labLastName";
+            labLastName.Size = new Size(75, 20);
+            labLastName.TabIndex = 2;
+            labLastName.Text = "LastName";
+            // 
+            // labClass
+            // 
+            labClass.AutoSize = true;
+            labClass.BackColor = SystemColors.ButtonFace;
+            labClass.Location = new Point(69, 252);
+            labClass.Name = "labClass";
+            labClass.Size = new Size(42, 20);
+            labClass.TabIndex = 3;
+            labClass.Text = "Class";
+            // 
+            // labEmail
+            // 
+            labEmail.AutoSize = true;
+            labEmail.BackColor = SystemColors.ButtonFace;
+            labEmail.Location = new Point(69, 322);
+            labEmail.Name = "labEmail";
+            labEmail.Size = new Size(46, 20);
+            labEmail.TabIndex = 4;
+            labEmail.Text = "Email";
+            // 
+            // labAddress
+            // 
+            labAddress.AutoSize = true;
+            labAddress.BackColor = SystemColors.ButtonFace;
+            labAddress.Location = new Point(69, 356);
+            labAddress.Name = "labAddress";
+            labAddress.Size = new Size(62, 20);
+            labAddress.TabIndex = 5;
+            labAddress.Text = "Address";
+            // 
+            // labAge
+            // 
+            labAge.AutoSize = true;
+            labAge.BackColor = SystemColors.ButtonFace;
+            labAge.Location = new Point(69, 401);
+            labAge.Name = "labAge";
+            labAge.Size = new Size(36, 20);
+            labAge.TabIndex = 6;
+            labAge.Text = "Age";
+            // 
+            // txtFirstName
+            // 
+            txtFirstName.Location = new Point(217, 123);
+            txtFirstName.Name = "txtFirstName";
+            txtFirstName.Size = new Size(125, 27);
+            txtFirstName.TabIndex = 7;
+            // 
+            // txtLastName
+            // 
+            txtLastName.Location = new Point(217, 187);
+            txtLastName.Name = "txtLastName";
+            txtLastName.Size = new Size(125, 27);
+            txtLastName.TabIndex = 8;
+            // 
+            // txtClass
+            // 
+            txtClass.Location = new Point(217, 242);
+            txtClass.Name = "txtClass";
+            txtClass.Size = new Size(125, 27);
+            txtClass.TabIndex = 9;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(217, 317);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(125, 27);
+            txtEmail.TabIndex = 10;
+            // 
+            // txtAddress
+            // 
+            txtAddress.Location = new Point(217, 356);
+            txtAddress.Name = "txtAddress";
+            txtAddress.Size = new Size(125, 27);
+            txtAddress.TabIndex = 11;
+            // 
+            // txtAge
+            // 
+            txtAge.Location = new Point(217, 412);
+            txtAge.Name = "txtAge";
+            txtAge.Size = new Size(125, 27);
+            txtAge.TabIndex = 12;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(51, 495);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(94, 29);
+            btnAdd.TabIndex = 13;
+            btnAdd.Text = "Create";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click_1;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(151, 495);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(94, 29);
+            btnUpdate.TabIndex = 14;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click_1;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(265, 495);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(94, 29);
+            btnDelete.TabIndex = 15;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click_1;
+            // 
+            // labId
+            // 
+            labId.AutoSize = true;
+            labId.BackColor = SystemColors.ButtonFace;
+            labId.Location = new Point(78, 86);
+            labId.Name = "labId";
+            labId.Size = new Size(22, 20);
+            labId.TabIndex = 16;
+            labId.Text = "Id";
+            // 
+            // txtId
+            // 
+            txtId.Location = new Point(215, 72);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(125, 27);
+            txtId.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            AcceptButton = btnAdd;
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1386, 693);
+            Controls.Add(txtId);
+            Controls.Add(labId);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnAdd);
+            Controls.Add(txtAge);
+            Controls.Add(txtAddress);
+            Controls.Add(txtEmail);
+            Controls.Add(txtClass);
+            Controls.Add(txtLastName);
+            Controls.Add(txtFirstName);
+            Controls.Add(labAge);
+            Controls.Add(labAddress);
+            Controls.Add(labEmail);
+            Controls.Add(labClass);
+            Controls.Add(labLastName);
+            Controls.Add(labFirstName);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label labFirstName;
+        private Label labLastName;
+        private Label labClass;
+        private Label labEmail;
+        private Label labAddress;
+        private Label labAge;
+        private TextBox txtFirstName;
+        private TextBox txtLastName;
+        private TextBox txtClass;
+        private TextBox txtEmail;
+        private TextBox txtAddress;
+        private TextBox txtAge;
+        private Button btnAdd;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private Label labId;
+        private TextBox txtId;
+    }
+}
---
+++ AccessDatabase/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace AccessDatabase
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=F:\ITUTC\KÌ 5\LTTQ\AppDesktop\UTC\Database1.accdb;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        // Hàm tải dữ liệu từ Access và hiển thị trên DataGridView
+        private void LoadData()
+        {
+            try
+            {
+                using (OleDbConnection connection = new OleDbConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM Student"; // Kiểm tra tên bảng
+                    OleDbCommand command = new OleDbCommand(query, connection);
+                    OleDbDataAdapter adapter = new OleDbDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    dataGridView1.DataSource = dataTable;
+                    connection.Close();
+
+                    // Kiểm tra số cột
+                    if (dataTable.Columns.Count != 7)
+                    {
+                        MessageBox.Show($"Số cột không đúng: {dataTable.Columns.Count}/7");
+                    }
+
+                    // Đảm bảo hiển thị tất cả cột
+                    dataGridView1.AutoGenerateColumns = true;
+                    dataGridView1.Columns["ID"].HeaderText = "ID";
+                    dataGridView1.Columns["FirstName"].HeaderText = "Tên";
+                    dataGridView1.Columns["LastName"].HeaderText = "Họ";
+                    dataGridView1.Columns["Address"].HeaderText = "Địa chỉ";
+                    dataGridView1.Columns["Class"].HeaderText = "Lớp";
+                    dataGridView1.Columns["Age"].HeaderText = "Tuổi";
+                    dataGridView1.Columns["Email"].HeaderText = "Email";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi tải dữ liệu: " + ex.Message + "\nStackTrace: " + ex.StackTrace);
+            }
+        }
+
+        // Sự kiện khi chọn một dòng trong DataGridView
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                try
+                {
+                    DataGridViewRow row = dataGridView1.SelectedRows[0];
+                    txtId.Text = row.Cells["ID"].Value?.ToString() ?? "";
+                    txtFirstName.Text = row.Cells["FirstName"].Value?.ToString() ?? "";
+                    txtLastName.Text = row.Cells["LastName"].Value?.ToString() ?? "";
+                    txtAddress.Text = row.Cells["Address"].Value?.ToString() ?? "";
+                    txtClass.Text = row.Cells["Class"].Value?.ToString() ?? "";
+                    txtAge.Text = row.Cells["Age"].Value?.ToString() ?? "";
+                    txtEmail.Text = row.Cells["Email"].Value?.ToString() ?? "";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi chọn dòng: " + ex.Message);
+                }
+            }
+        }
+
+        // Hàm xóa các ô nhập liệu
+        private void ClearInputs()
+        {
+            txtId.Text = "";
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtAddress.Text = "";
+            txtClass.Text = "";
+            txtAge.Text = "";
+            txtEmail.Text = "";
+        }
+
+        // Nút Thêm
+        private void btnAdd_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtFirstName.Text) || string.IsNullOrEmpty(txtLastName.Text) ||
+                string.IsNullOrEmpty(txtAddress.Text) || string.IsNullOrEmpty(txtClass.Text) ||
+                string.IsNullOrEmpty(txtAge.Text) || string.IsNullOrEmpty(txtEmail.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!");
+                return;
+            }
+
+            if (!int.TryParse(txtAge.Text, out int age) || age <= 0)
+            {
+                MessageBox.Show("Tuổi phải là số nguyên dương!");
+                return;
+            }
+
+            try
+            {
+                using (OleDbConnection connection = new OleDbConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "INSERT INTO Student (FirstName, LastName, Address, Class, Age, Email) " +
+                                  "VALUES (@FirstName, @LastName, @Address, @Class, @Age, @Email)";
+                    OleDbCommand command = new OleDbCommand(query, connection);
+                    command.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+                    command.Parameters.AddWithValue("@LastName", txtLastName.Text);
+                    command.Parameters.AddWithValue("@Address", txtAddress.Text);
+                    command.Parameters.AddWithValue("@Class", txtClass.Text);
+                    command.Parameters.AddWithValue("@Age", age);
+                    command.Parameters.AddWithValue("@Email", txtEmail.Text);
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Thêm học sinh thành công!");
+                    connection.Close();
+                    LoadData();
+                    ClearInputs();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi thêm: " + ex.Message + "\nStackTrace: " + ex.StackTrace);
+            }
+        }
+
+        // Nút Sửa
+        private void btnUpdate_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtId.Text))
+            {
+                MessageBox.Show("Vui lòng chọn một học sinh để sửa!");
+                return;
+            }
+
+            if (!int.TryParse(txtAge.Text, out int age) || age <= 0)
+            {
+                MessageBox.Show("Tuổi phải là số nguyên dương!");
+                return;
+            }
+
+            try
+            {
+                using (OleDbConnection connection = new OleDbConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "UPDATE Student SET FirstName = @FirstName, LastName = @LastName, " +
+                                  "Address = @Address, Class = @Class, Age = @Age, Email = @Email WHERE ID = @ID";
+                    OleDbCommand command = new OleDbCommand(query, connection);
+                    command.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+                    command.Parameters.AddWithValue("@LastName", txtLastName.Text);
+                    command.Parameters.AddWithValue("@Address", txtAddress.Text);
+                    command.Parameters.AddWithValue("@Class", txtClass.Text);
+                    command.Parameters.AddWithValue("@Age", age);
+                    command.Parameters.AddWithValue("@Email", txtEmail.Text);
+                    command.Parameters.AddWithValue("@ID", txtId.Text);
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Cập nhật học sinh thành công!");
+                    connection.Close();
+                    LoadData();
+                    ClearInputs();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi sửa: " + ex.Message + "\nStackTrace: " + ex.StackTrace);
+            }
+        }
+
+        // Nút Xóa
+        private void btnDelete_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtId.Text))
+            {
+                MessageBox.Show("Vui lòng chọn một học sinh để xóa!");
+                return;
+            }
+
+            if (MessageBox.Show("Bạn có chắc muốn xóa học sinh này?", "Xác nhận xóa", MessageBoxButtons.YesNo) == DialogResult.No)
+            {
+                return;
+            }
+
+            try
+            {
+                using (OleDbConnection connection = new OleDbConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "DELETE FROM Student WHERE ID = @ID";
+                    OleDbCommand command = new OleDbCommand(query, connection);
+                    command.Parameters.AddWithValue("@ID", txtId.Text);
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Xóa học sinh thành công!");
+                    connection.Close();
+                    LoadData();
+                    ClearInputs();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi xóa: " + ex.Message + "\nStackTrace: " + ex.StackTrace);
+            }
+        }
+
+        // Sự kiện click ô trong DataGridView (chỉ chọn dòng, không xóa)
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                txtId.Text = row.Cells["ID"].Value?.ToString() ?? "";
+                txtFirstName.Text = row.Cells["FirstName"].Value?.ToString() ?? "";
+                txtLastName.Text = row.Cells["LastName"].Value?.ToString() ?? "";
+                txtAddress.Text = row.Cells["Address"].Value?.ToString() ?? "";
+                txtClass.Text = row.Cells["Class"].Value?.ToString() ?? "";
+                txtAge.Text = row.Cells["Age"].Value?.ToString() ?? "";
+                txtEmail.Text = row.Cells["Email"].Value?.ToString() ?? "";
+            }
+        }
+    }
+}
